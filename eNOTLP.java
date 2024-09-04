6fba4e726ab96b9b12b0044d1a2c3f81a4ea1f6b
rahmadini11 <rrhmaandini26@gmail.com>
2024-09-04 15:26:03 +0700

    Add files via upload

---
+++ eNOTLP.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+class eNOTLP {
+
+    static void setText(String NOTLP) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
